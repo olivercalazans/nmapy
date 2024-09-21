b79a8097529ea91d0bbf497e8f90f02549c698e0
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-21 20:11:55 -0300

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from network import *
 
 class Strategy(ABC):
     @abstractmethod
@@ -7,18 +8,18 @@ class Strategy(ABC):
 
 
 class Command_List_Strategy(Strategy):
-    def execute(self, server, client_port:int, arguments:str):
+    def execute(self, arguments:str):
         result = server._get_command_list()
         return ('svc', result)
 
 
 class Portscan_Strategy(Strategy):
-    def execute(self, server, client_port:int, arguments:str):
-        result = server._portscan(arguments)
+    def execute(self, arguments:str):
+        result = Network._portscan(arguments)
         return ('svc', result)
     
 
 class IP_Strategy(Strategy):
-    def execute(self, server, client_port:int, arguments:str):
-        result = server._ip(arguments)
+    def execute(self, arguments:str):
+        result = Network._ip(arguments)
         return ('svc', result)
