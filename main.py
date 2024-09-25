91ee781649a491c1651b04f164da8475b428292b
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-24 23:28:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ class Main:
 
     def __init__(self) -> None:
         self._stop_flag = False
-        self._create_directory(self._get_directory)
+        self._create_directory(self._get_directory())
 
 
     @staticmethod
-    def _create_directory(_directory:str) -> None:
-        try:   os.mkdir(_directory)
+    def _create_directory(directory:str) -> None:
+        try:   os.mkdir(directory)
         except FileExistsError: print('The directory already exists')
         except Exception as error: print(f'Error creating directory: {error}')
         else:  print('Directory created')
@@ -49,28 +49,30 @@ class Main:
 
     def _loop(self) -> None:
         while not self._stop_flag:
-            print('Write "help" to see the commands')
+            print('Write "help" to see the commands ' + '=' * 20)
             input_data = input('> ')
             command = self._separate_command_key_from_arguments(input_data)
-            result  = self._get_result(command)
+            result  = self._check_if_the_method_exists(command)
+            for line in result: print(line)
 
 
     @staticmethod
-    def _separate_command_key_from_arguments(input_data:str) -> tuple[str, str, str]:
+    def _separate_command_key_from_arguments(input_data:str) -> str:
         command = input_data.split(' ')
-        return command
+        return command[0]
 
 
-    def _check_if_the_method_exists(self, command) -> None:
+    def _check_if_the_method_exists(self, command:str) -> str:
         if command in self._get_strategy_dictionary():
             result = self._get_result(command)
         else:
-            result = 'Command not found'
+            result = 'Invalid command'
         return result
 
 
-    def _get_result(self, command) -> None:
-        ...
+    def _get_result(self, command:str, arguments:str = None) -> str:
+        strategy = self._get_strategy_dictionary().get(command)
+        return strategy.execute(arguments)
        
 
 
