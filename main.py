0aaff1aecc3b9813f09d073f93d4aab6e7d257a5
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-26 17:51:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ import os, platform
 from strategy import *
 
 class Main:
-    DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-    if platform.system() == 'Windows': DIRECTORY += '\\wfiles\\'
-    elif platform.system() == 'Linux': DIRECTORY += '/wfiles/'
-
-
     STRATEGY_DICTIONARY = {
         "help":  Command_List_Strategy(),
         "pscan": Portscan_Strategy(),
@@ -16,20 +11,6 @@ class Main:
 
     def __init__(self) -> None:
         self._stop_flag = False
-        self._create_directory(self._get_directory())
-
-
-    @staticmethod
-    def _create_directory(directory:str) -> None:
-        try:   os.mkdir(directory)
-        except FileExistsError: print('The directory already exists')
-        except Exception as error: print(f'Error creating directory: {error}')
-        else:  print('Directory created')
-
-
-    @classmethod
-    def _get_directory(cls) -> str:
-        return cls.DIRECTORY
     
 
     @classmethod
@@ -52,8 +33,7 @@ class Main:
             print('\nWrite "help" to see the commands ' + '=' * 20)
             input_data         = input('>> ').split(' ')
             command, arguments = self._separates_command_from_arguments(input_data)
-            result             = self._check_if_the_method_exists(command, arguments)
-            self._display(result)
+            self._check_if_the_method_exists(command, arguments)
 
 
     @staticmethod
@@ -63,22 +43,16 @@ class Main:
         return (command, arguments)
 
 
-    def _check_if_the_method_exists(self, command:str, arguments:tuple) -> str:
+    def _check_if_the_method_exists(self, command:str, arguments:tuple) -> None:
         if command in self._get_strategy_dictionary():
-            result = self._get_result(command, arguments)
+            self._get_result(command, arguments)
         else:
-            result = 'Invalid command'
-        return result
+            print('Invalid command')
 
 
-    def _get_result(self, command:str, arguments:str = None) -> str:
+    def _get_result(self, command:str, arguments:str) -> None:
         strategy = self._get_strategy_dictionary().get(command)
-        return strategy.execute(arguments)
-       
-
-    @staticmethod
-    def _display(data:list) -> None:
-        for line in data: print(line)
+        strategy.execute(arguments)
 
 
 
