c2633763716a28cbd4b4f6703926cfe034c28244
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-28 15:04:35 -0300

    feat: class added to color some text

--- network.py
+++ network.py
@@ -1,10 +1,11 @@
 import socket
+from auxiliary import *
 
 class Network:
     @staticmethod
-    def _get_ip_by_name(host_name:str) -> str:
-        try:    ip = socket.gethostbyname(host_name)
-        except: ip = 'ERROR. Invalid hostname'
+    def _get_ip_by_name(hostname:str) -> str:
+        try:    ip = socket.gethostbyname(hostname)
+        except: ip = f'{Aux._yellow("ERROR")}: Invalid hostname ({hostname})'
         return  ip
 
 
@@ -16,25 +17,22 @@ class Network:
 
     # PORTSCAN -----------------------------------------------------------------------------------------------
     @staticmethod
-    def _portscan(host:str, ports:list) -> None:
+    def _portscan(host:str, ports:dict) -> None:
         ip = Network._get_ip_by_name(host)
         try:   Network._scan(ip, ports)
-        except socket.gaierror: print('ERROR: An error occurred in resolving the host')
-        except socket.error:    print(f'ERROR: It was not possible to connect to {host}')
-        except Exception as error: print(f'ERROR: {error}') 
+        except socket.gaierror: print(f'{Aux._yellow("ERROR")}: An error occurred in resolving the host')
+        except socket.error:    print(f'{Aux._yellow("ERROR")}: It was not possible to connect to {host}')
+        except Exception as error: print(f'{Aux._red("ERROR")}: {error}') 
 
     
     @staticmethod
     def _scan(ip:str, ports:dict) -> list:
-        data = list()
         for port in ports.keys():
             portscan_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             portscan_socket.settimeout(3)
             result = portscan_socket.connect_ex((ip, port))
-            status = 'Closed'
-            if result == 0: status = 'Opened'
+            status = Aux._red('Closed')
+            if result == 0: status = Aux._green('Opened')
             message = f' Port {port:>4} : {ports[port]} (STATUS -> {status})'
             print(message)
-            data.append(message)
             portscan_socket.close()
-        return data
