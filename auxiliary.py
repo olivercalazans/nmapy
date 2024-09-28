bfb5fe779031c11684965f0112588b57a68f7389
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-28 15:05:00 -0300

    Add files via upload

---
+++ auxiliary.py
@@ -0,0 +1,14 @@
+class Aux:
+    @staticmethod
+    def _red(message):
+        return '\033[31m' + message + '\033[0m'
+    
+    @staticmethod
+    def _green(message):
+        return '\033[32m' + message + '\033[0m'
+    
+    def _yellow(message):
+        return '\033[33m' + message + '\033[0m'
+    
+    def _orange(message):
+        return '\033[38;5;214m' + message + '\033[0m'
