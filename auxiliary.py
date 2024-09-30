01e34b0ab4bf0b265a6ade069c13e0ac32b58590
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-09-29 22:24:29 -0300

    Update auxiliary.py

--- auxiliary.py
+++ auxiliary.py
@@ -1,26 +1,34 @@
 class Command_List:
     def _execute(self, arguments:str) -> None:
         commands = (
-            f'{Aux._green("pscan")}.....: Port scanner',
-            f'{Aux._green("ip")}........: Get IP by name',
-            f'{Aux._green("netscan")}...: Network scanner'
+            f'{Aux.green("pscan")}.....: Port scanner',
+            f'{Aux.green("ip")}........: Get IP by name',
+            f'{Aux.green("netscan")}...: Network scanner'
         )
         for i in commands: print(i)
 
 
 class Aux:
     @staticmethod
-    def _red(message):
+    def red(message) -> str:
         return '\033[31m' + message + '\033[0m'
     
     @staticmethod
-    def _green(message):
+    def green(message) -> str:
         return '\033[32m' + message + '\033[0m'
     
     @staticmethod
-    def _yellow(message):
+    def yellow(message) -> str:
         return '\033[33m' + message + '\033[0m'
     
     @staticmethod
-    def _orange(message):
+    def orange(message) -> str:
         return '\033[38;5;214m' + message + '\033[0m'
+    
+    @staticmethod
+    def display_unexpected_error(error:str) -> str:
+        return Aux.red('Unexpect error') + f'\nERROR: {error}'
+    
+    @staticmethod
+    def display_error(message:str) -> str:
+        return Aux.yellow('ERROR: ') + message
