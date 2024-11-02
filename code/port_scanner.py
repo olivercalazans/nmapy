07e5441c62b0135b93d5d9ba660eb372c4b5e7d5
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-11-02 11:56:25 -0300

    Update port_scanner.py

--- code/port_scanner.py
+++ code/port_scanner.py
@@ -7,67 +7,25 @@ import socket, ipaddress, random, time, threading, sys
 from scapy.all import IP, TCP
 from scapy.all import sr, sr1, send
 from scapy.all import conf, packet
-from network import *
+from network   import *
 from auxiliary import Aux, Argument_Parser_Manager
 
 
 class Port_Scanner:
     """Performs a port scan on a specified host."""
 
-    FLAGS                = None
-    ASYNC_RESPONSES      = list()
-    LOCK                 = threading.Lock()
-    REAL_PACKET_RESPONSE = None
-    MY_IP_ADDRESS        = None
-
-    @staticmethod
-    def _execute(database, data:list) -> None:
-        """ Executes the port scanning process with error handling."""
-        try:
-            host       = Port_Scanner._get_argument_and_flags(database.parser_manager, data)
-            ports      = Port_Scanner._prepare_ports(Port_Scanner.FLAGS['ports'])
-            target_ip  = Network._get_ip_by_name(host, True)
-            interface  = Network._select_interface()
-            conf.iface = interface
-            conf.verb  = 0 if not Port_Scanner.FLAGS['verbose'] else 1
-            responses  = Port_Scanner._get_result_by_transmission_method(target_ip, ports, interface)
-            Port_Scanner._process_responses(responses)
-        except SystemExit:         print(Aux.display_invalid_missing())
-        except KeyboardInterrupt:  print(Aux.orange("Process stopped"))
-        except socket.gaierror:    print(Aux.display_error('An error occurred in resolving the host'))
-        except socket.error:       print(Aux.display_error(f'It was not possible to connect to "{host}"'))
-        except Exception as error: print(Aux.display_unexpected_error(error))
-
-
-    @staticmethod
-    def _get_argument_and_flags(parser_manager:Argument_Parser_Manager, data:list) -> str:
-        """Parses and retrieves the hostname, port, and verbosity flag from the arguments."""
-        arguments = parser_manager._parse("PortScanner", data)
-        Port_Scanner.FLAGS = {
-            'ports':   arguments.port,
-            'verbose': arguments.verbose,
-            'random':  arguments.random_order,
-            'delay':   arguments.random_delay,
-            'decoy':   arguments.decoy,
-        }
-        return (arguments.host)
-
-
-    @staticmethod
-    def _prepare_ports(input_ports:str) -> dict:
-        """Prepares the port or ports to be scanned."""
-        ports = Port_Scanner._get_ports()
-        if input_ports:
-            return {int(valor): None for valor in input_ports.split(",")}
-        elif Port_Scanner.FLAGS.get('random'):
-            return dict(random.sample(list(ports.items()), len(ports)))
-        return ports
-
-
-    @staticmethod
-    def _get_ports() -> dict:
-        """Returns a dictionary of common ports and their services."""
-        return { 
+    def __init__(self) -> None:
+        self._host                 = None
+        self._flags                = None
+        self._ports_to_be_used     = None
+        self._target_ip            = None
+        self._interface            = None
+        self._async_responses      = list()
+        self._lock                 = threading.Lock()
+        self._real_packet_response = None
+        self._my_ip_address        = None
+        self._responses            = None
+        self._all_ports            = { 
             21   : 'FTP - File Transfer Protocol',  
             22   : 'SSH - Secure Shell',  
             23   : 'Telnet',  
@@ -84,34 +42,70 @@ class Port_Scanner:
             8080 : 'Jakarta Tomcat',
             27017: 'MongoDB'
         }
+        
+
+    def _execute(self, database, data:list) -> None:
+        """ Executes the port scanning process with error handling."""
+        try:
+            self._get_argument_and_flags(database.parser_manager, data)
+            self._target_ip = Network._get_ip_by_name(self._host, True)
+            self._interface = Network._select_interface()
+            conf.iface      = self._interface
+            conf.verb       = 0 if not self._flags['verbose'] else 1
+            self._get_result_by_transmission_method()
+            self._process_responses()
+        except SystemExit:         print(Aux.display_invalid_missing())
+        except KeyboardInterrupt:  print(Aux.orange("Process stopped"))
+        except socket.gaierror:    print(Aux.display_error('An error occurred in resolving the host'))
+        except socket.error:       print(Aux.display_error(f'It was not possible to connect to "{self._host}"'))
+        except Exception as error: print(Aux.display_unexpected_error(error))
 
 
-    @staticmethod
-    def _get_result_by_transmission_method(target_ip:str, ports:dict, interface:str) -> list:
+    def _get_argument_and_flags(self, parser_manager:Argument_Parser_Manager, data:list) -> None:
+        """Parses and retrieves the hostname, port, and verbosity flag from the arguments."""
+        arguments   = parser_manager._parse("PortScanner", data)
+        self._host  = arguments.host
+        self._flags = {
+            'ports':   arguments.port,
+            'verbose': arguments.verbose,
+            'random':  arguments.random_order,
+            'delay':   arguments.random_delay,
+            'decoy':   arguments.decoy,
+        }
+
+
+    def _prepare_ports(self, specified_ports = None) -> None:
+        """Prepares the port or ports to be scanned."""
+        if specified_ports: self._ports_to_be_used = [int(valor) for valor in specified_ports.split(",")]
+        else:               self._ports_to_be_used = list(self._all_ports.keys())
+        if self._flags['random']: 
+            self._ports_to_be_used = random.sample(self._ports_to_be_used, len(self._ports_to_be_used))
+
+
+    def _get_result_by_transmission_method(self) -> list:
         """Retrieves the scan results based on the specified transmission method."""
-        decoy = Port_Scanner.FLAGS['decoy']
-        if isinstance(decoy, str):
-            response = Port_Scanner._perform_decoy_method(decoy, interface, target_ip)
+        if isinstance(self._flags['decoy'], str):
+            response = self._perform_decoy_method()
         else:
-            response = Port_Scanner._perform_normal_scan(target_ip, ports)
-        return response
+            response = self._perform_normal_scan()
+        self._responses = response
 
 
     # NORMAL SCAN --------------------------------------------------------------------------------------------
-    @staticmethod
-    def _perform_normal_scan(target_ip:str, port_dictionary:dict) -> list:
+    def _perform_normal_scan(self) -> list:
         """Performs a normal scan on the specified target IP address."""
-        delay   = Port_Scanner.FLAGS['delay']
-        packets = Port_Scanner._create_packets(target_ip, port_dictionary)
-        if delay: responses    = Port_Scanner._async_sending(packets, delay)
-        else:     responses, _ = Port_Scanner._send_packets(packets)
+        self._prepare_ports(self._flags['ports'])
+        packets = [self._create_tpc_ip_packet(port) for port in self._ports_to_be_used]
+        if self._flags['delay']: 
+            responses = self._async_sending(packets)
+        else: 
+            responses, _ = self._send_packets(packets)
         return responses
 
 
-    @staticmethod
-    def _create_packets(target_ip:str, ports:dict, source_ip=None) -> list:
-        """Creates the TCP SYN packets to be sent for scanning the specified ports."""
-        return [IP(src=source_ip, dst=target_ip) / TCP(dport=port, flags="S") for port in ports.keys()]
+    def _create_tpc_ip_packet(self, port, source_ip=None) -> list:
+        """Creates the TCP SYN packet"""
+        return IP(src=source_ip, dst=self._target_ip) / TCP(dport=port, flags="S")
 
 
     @staticmethod
@@ -121,13 +115,12 @@ class Port_Scanner:
         return (responses, unanswered)
 
 
-    @staticmethod
-    def _async_sending(packets:list, delay:bool|str) -> list:
+    def _async_sending(self, packets:list) -> None:
         """Sends TCP packets in concurrent threads with an optional delay between each send."""
-        delay   = Port_Scanner._get_delay_time_list(delay, len(packets))
+        delay   = self._get_delay_time_list(self._flags['delay'], len(packets))
         threads = []
         for index ,packet in enumerate(packets):
-            thread = threading.Thread(target=Port_Scanner._async_send_packet, args=(packet,))
+            thread = threading.Thread(target=self._async_send_packet, args=(packet,))
             threads.append(thread)
             thread.start()
             sys.stdout.write(f'\rPacket sent: {index}/{len(packets)} - {delay[index]:.2}s')
@@ -136,7 +129,7 @@ class Port_Scanner:
         for thread in threads:
             thread.join()
         print('\n')
-        return Port_Scanner.ASYNC_RESPONSES
+        return self._async_responses
 
 
     @staticmethod
@@ -155,31 +148,28 @@ class Port_Scanner:
         return [values[0] for _ in range(packet_number)]
 
 
-    @staticmethod
-    def _async_send_packet(packet:packet) -> None:
+    def _async_send_packet(self, packet:packet) -> None:
         """Sends a single TCP SYN packet asynchronously and stores the response."""
         response = sr1(packet, timeout=5, verbose=False)
-        with Port_Scanner.LOCK:
-            Port_Scanner.ASYNC_RESPONSES.append((packet, response))
+        with self._lock:
+            self._async_responses.append((packet, response))
 
 
     # DECOY METHODS ------------------------------------------------------------------------------------------    
-    @staticmethod
-    def _perform_decoy_method(port:int, interface:str, target_ip:str) -> list:
+    def _perform_decoy_method(self) -> list:
         """Performs a decoy scan method using the specified port and network interface."""
-        ports   = Port_Scanner._prepare_ports(port)
-        my_ip   = Network._get_ip_address(interface)
-        netmask = Network._get_subnet_mask(interface)
-        ips     = Port_Scanner._prepare_decoy_and_real_ips(my_ip, netmask)
-        Port_Scanner._send_decoy_and_real_packets(ips, my_ip, target_ip, ports)
-        return Port_Scanner.REAL_PACKET_RESPONSE
+        self._prepare_ports(self._flags['decoy'])
+        self._my_ip_address = Network._get_ip_address(self._interface)
+        netmask             = Network._get_subnet_mask(self._interface)
+        ips                 = self._prepare_decoy_and_real_ips(netmask)
+        self._send_decoy_and_real_packets(ips)
+        return self._real_packet_response
 
 
-    @staticmethod
-    def _prepare_decoy_and_real_ips(my_ip:str, subnet_mask:str) -> None:
+    def _prepare_decoy_and_real_ips(self, subnet_mask:str) -> list:
         """ Prepares a list of decoy IPs and adds the real IP to the list."""
-        decoy_packets = Port_Scanner._generate_random_ip_in_subnet(my_ip, subnet_mask)
-        return Port_Scanner._add_real_packet(decoy_packets, my_ip)
+        decoy_packets = self._generate_random_ip_in_subnet(self._my_ip_address, subnet_mask)
+        return self._add_real_packet(decoy_packets, self._my_ip_address)
 
 
     @staticmethod
@@ -195,50 +185,45 @@ class Port_Scanner:
     def _add_real_packet(decoy_ips:list, my_ip:str) -> list:
         """Inserts the real IP address into a list of decoy IPs at a random position."""
         packet_number = len(decoy_ips)
-        index         = random.randint(packet_number // 2, packet_number - 1)
-        decoy_ips.insert(index, my_ip)
+        real_ip_index = random.randint(packet_number // 2, packet_number - 1)
+        decoy_ips.insert(real_ip_index, my_ip)
         return decoy_ips
 
 
-    @staticmethod
-    def _send_decoy_and_real_packets(ip_list:list, my_ip:str ,target_ip:str, ports:int) -> None:
+    def _send_decoy_and_real_packets(self, ip_list:list) -> None:
         """Sends both decoy and real packets to the specified target IP address."""
         for ip in ip_list:
-            if ip == my_ip:
-                thread = threading.Thread(target=Port_Scanner._send_real_packet, args=(my_ip, target_ip, ports))
+            if ip == self._my_ip_address:
+                thread = threading.Thread(target=self._send_real_packet)
                 thread.start()
             else:
-                Port_Scanner._send_decoy_packet(ip, target_ip, ports)
+                self._send_decoy_packet(ip)
             delay = random.uniform(1, 3)
             print(f'{Aux.green("Packet sent")}: {ip}, Delay: {delay:.2}')
             time.sleep(delay)
 
 
-    @staticmethod
-    def _send_real_packet(my_ip:str, target_ip:str, port:int) -> None:
+    def _send_real_packet(self) -> None:
         """Sends a real TCP SYN packet to the specified target IP address."""
-        real_packet = Port_Scanner._create_packets(target_ip, port, my_ip)[0]
+        real_packet = self._create_tpc_ip_packet(self._ports_to_be_used[0])
         response    = sr1(real_packet, verbose=0)
-        Port_Scanner.REAL_PACKET_RESPONSE = [(real_packet, response)]
+        self._real_packet_response = [(real_packet, response)]
 
 
-    @staticmethod
-    def _send_decoy_packet(decoy_ip:str, target_ip:str, port:int) -> None:
+    def _send_decoy_packet(self, decoy_ip:str) -> None:
         """Sends a decoy TCP SYN packet to the specified target IP address."""
-        decoy_packet = Port_Scanner._create_packets(target_ip, port, decoy_ip)[0]
+        decoy_packet = self._create_tpc_ip_packet(self._ports_to_be_used, decoy_ip)
         send(decoy_packet, verbose=0)
 
 
     # PROCESS DATA -------------------------------------------------------------------------------------------
-    @staticmethod
-    def _process_responses(responses:list) -> None:
+    def _process_responses(self) -> None:
         """Processes the scan responses and displays the results."""
-        all_ports = Port_Scanner._get_ports()
-        for sent, received in responses:
+        for sent, received in self._responses:
             port           = sent[TCP].dport
             response_flags = received[TCP].flags if received else None
-            description    = all_ports[port] if port in all_ports else 'Generic Port'
-            Port_Scanner._display_result(response_flags, port, description)
+            description    = self._all_ports[port] if port in self._all_ports else 'Generic Port'
+            self._display_result(response_flags, port, description)
 
 
     @staticmethod
