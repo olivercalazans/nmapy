acb6df8175005cfc6da4302e191e4f6c4d411063
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-12-03 23:11:42 -0300

    Update os_fingerprint.py

--- code/os_fingerprint.py
+++ code/os_fingerprint.py
@@ -4,7 +4,7 @@
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
 
-from scapy.all import ICMP, IP, TCP, UDP, Packet
+from scapy.all import ICMP, IP, TCP, UDP, Raw, Packet
 from network   import Network
 from auxiliary import Argument_Parser_Manager, Color
 
@@ -15,8 +15,8 @@ class OS_Fingerprint:
         self._icmp_packet = None    # Packet
         self._tcp_packet  = None    # Packet
         self._udp_packet  = None    # Packet
-        self._icmp_result = None
-        self._tcp_result  = None
+        self._icmp_result = list()
+        self._tcp_result  = list()
         self._udp_result  = dict()
 
 
@@ -42,27 +42,51 @@ class OS_Fingerprint:
     # ICMP ---------------------------------------------------------------------------------------------------
     def _perform_icmp_fingerprint(self) -> None:
         """Performs OS fingerprinting using ICMP by sending an ICMP packet to the target."""
-        print(f"Performing OS fingerprinting with ICMP on {self._target_ip}...")
-        packet   = Network._create_icmp_ip_packet(self._target_ip)
-        response = Network._send_and_receive_single_layer3_packet(packet)
-        if response: self._analyze_icmp_response(response)
-        else: self._icmp_result = "No ICMP response received. Host unreachable or potentially blocked by firewall."
+        print(f'Performing OS fingerprinting with ICMP on {self._target_ip}...')
+        packets   = [self._create_first_icmp_packet(self._target_ip), self._create_second_icmp_packet(self._target_ip)]
+        responses = [Network._send_and_receive_single_layer3_packet(pkt) for pkt in packets]
+        if responses: self._analyze_icmp_response(responses)
+        else: self._icmp_result.append('No ICMP response received. Host unreachable or potentially blocked by firewall.')
 
 
-    def _analyze_icmp_response(self, icmp_packet:Packet) -> None:
+    @staticmethod
+    def _create_first_icmp_packet(target_ip:str) -> Packet:
+        packet_1 = Network._create_icmp_ip_packet(target_ip)
+        packet_1[IP].flags  = 'DF'
+        packet_1[IP].tos    = 0
+        packet_1[ICMP].type = 8
+        packet_1[ICMP].code = 9
+        packet_1[ICMP].id   = 12345
+        packet_1[ICMP].seq  = 295
+        return packet_1 / Raw(load=b'\x00' * 120)
+
+
+    @staticmethod
+    def _create_second_icmp_packet(target_ip:str) -> Packet:
+        packet_2 = Network._create_icmp_ip_packet(target_ip)
+        packet_2[IP].tos    = 4
+        packet_2[ICMP].type = 8
+        packet_2[ICMP].code = 0
+        packet_2[ICMP].id   = 12346
+        packet_2[ICMP].seq  = 296
+        return packet_2 / b'\x00' * 150
+
+
+    def _analyze_icmp_response(self, icmp_packets:Packet) -> None:
         """Analyzes an ICMP response to extract relevant packet characteristics."""
-        ttl_result       = self._analyze_ttl(icmp_packet.ttl)
-        icmp_type_result = self._analyse_icmp_type_and_code(icmp_packet)
-        self._icmp_result = f'\t{ttl_result}\n\t{icmp_type_result}'
+        for packet in icmp_packets:
+            ttl_result        = self._analyze_ttl(packet.ttl)
+            icmp_type_result  = self._analyse_icmp_type_and_code(packet)
+            self._icmp_result.append(f'\t{ttl_result}\n\t{icmp_type_result}')
 
 
     @staticmethod
     def _analyze_ttl(ttl:int) -> str:
         """Identifies the operating system based on the TTL value from an ICMP response."""
-        if   ttl <= 64:  ttl_result = f"Likely OS: Linux/Android (TTL={ttl})"
-        elif ttl <= 128: ttl_result = f"Likely OS: Windows (TTL={ttl})"
-        elif ttl > 128:  ttl_result = f"Likely OS: iOS or other system with higher TTL (TTL={ttl})"
-        else:            ttl_result = f"Unknown system (TTL={ttl})"
+        if   ttl <= 64:  ttl_result = f'Likely OS: Linux/Android (TTL={ttl})'
+        elif ttl <= 128: ttl_result = f'Likely OS: Windows (TTL={ttl})'
+        elif ttl > 128:  ttl_result = f'Likely OS: iOS or other system with higher TTL (TTL={ttl})'
+        else:            ttl_result = f'Unknown system (TTL={ttl})'
         return ttl_result
 
 
@@ -72,49 +96,88 @@ class OS_Fingerprint:
         if icmp_packet.haslayer(ICMP):
             icmp_type = icmp_packet[ICMP].type
             icmp_code = icmp_packet[ICMP].code
-            if   icmp_type == 0:  icmp_type_and_code = "Echo reply received: Target is responding to ping."
-            elif icmp_type == 3:  icmp_type_and_code = f"Destination Unreachable (Code {icmp_code}): Potential firewall or routing issue."
-            elif icmp_type == 11: icmp_type_and_code = "Time Exceeded: Possible routing or firewall filtering."
-            else:                 icmp_type_and_code = f"ICMP Type {icmp_type} (Code {icmp_code}): Unknown response."
+            if   icmp_type == 0:  icmp_type_and_code = 'Echo reply received: Target is responding to ping.'
+            elif icmp_type == 3:  icmp_type_and_code = f'Destination Unreachable (Code {icmp_code}): Potential firewall or routing issue.'
+            elif icmp_type == 11: icmp_type_and_code = 'Time Exceeded: Possible routing or firewall filtering.'
+            else:                 icmp_type_and_code = f'ICMP Type {icmp_type} (Code {icmp_code}): Unknown response.'
         else:
-            icmp_type_and_code = f"No ICMP layer in response"
+            icmp_type_and_code = f'No ICMP layer in response'
         return icmp_type_and_code
 
 
     # TCP ----------------------------------------------------------------------------------------------------
     def _perform_tcp_fingerprint(self) -> None:
         """Performs OS fingerprinting using TCP by sending a SYN packet to the target."""
-        print(f"Performing OS fingerprinting with TCP on {self._target_ip}...")
-        packet   = Network._create_tpc_ip_packet(self._target_ip, 80)
-        response = Network._send_and_receive_single_layer3_packet(packet)
-        if response and response.haslayer(TCP):
-            self._identify_os_by_tcp(response)
-        else:
-            print("No TCP response received.")
+        print(f'Performing OS fingerprinting with TCP on {self._target_ip}...')
+        packets   = self._get_sequence_packets(self._target_ip) + self._get_test_packets(self._target_ip)
+        responses = Network._send_and_receive_multiple_layer3_packets(packets)
+        answers   = [i.answer for i in responses] 
+        self._process_tcp_responses(answers)
+
+
+    @staticmethod
+    def _get_sequence_packets(target_ip:str) -> list:
+        PACKET = Network._create_tpc_ip_packet(target_ip, 12345)
+        return [
+            PACKET / TCP(window=1,   options=[('WScale', 10), ('NOP', None), ('MSS', 1460), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b''),]),
+            PACKET / TCP(window=63,  options=[('MSS', 1400),  ('WScale', 0), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('EOL', None)]),
+            PACKET / TCP(window=4,   options=[('Timestamp', (0xFFFFFFFF, 0)), ('NOP', None), ('NOP', None), ('WScale', 5), ('NOP', None), ('MSS', 640)]),
+            PACKET / TCP(window=4,   options=[('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
+            PACKET / TCP(window=16,  options=[('MSS', 536), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
+            PACKET / TCP(window=512, options=[('MSS', 265), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0))])
+            ]
+
+
+    @staticmethod
+    def _get_test_packets(target_ip:str) -> Packet:
+        TCP_OPTIONS  = [('NOP', None), ('MSS', 265), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b'')]
+        return [
+            IP(dst=target_ip, flags='DF') / TCP(window=128,   flags='S', options=[('WScale', 10)] + TCP_OPTIONS),
+            IP(dst=target_ip)       /       TCP(window=256,   flags='A', options=[('WScale', 10)] + TCP_OPTIONS),
+            IP(dst=target_ip, flags='DF') / TCP(window=1024,  flags='A', options=[('WScale', 10)] + TCP_OPTIONS),
+            IP(dst=target_ip)       /       TCP(window=31337, flags='S', options=[('WScale', 10)] + TCP_OPTIONS, dport=12345),
+            IP(dst=target_ip, flags='DF') / TCP(window=32768, flags='A', options=[('WScale', 10)] + TCP_OPTIONS, dport=12345),
+            IP(dst=target_ip)       /       TCP(window=65535, flags='S', options=[('WScale', 15)] + TCP_OPTIONS, dport=12345)
+        ]
+
+
+    def _process_tcp_responses(self, responses:Packet) -> None:
+        for packet in responses:
+            if packet and packet.haslayer(TCP):
+                self._identify_os_by_tcp(packet)
+            else:
+                self._tcp_result.append(f'No TCP response received. >> {packet}')
 
 
     def _identify_os_by_tcp(self, response:Packet) -> None:
         """Identifies the operating system based on the TCP flags from the response."""
-        if   response[TCP].flags == 18: os = "TCP response received: Likely Windows or Linux-based system."
-        elif response[TCP].flags == 4:  os = "TCP reset received: Likely a filtered firewall or closed port."
-        elif response[TCP].flags == 2:  os = "SYN response received: Likely a system with an open port (TCP handshake)."
-        elif response[TCP].flags == 1:  os = "FIN response received: System may be closing the connection (end of session)."
-        else:                           os = "Unknown TCP flag combination, further analysis required."
-        self._tcp_result = f'\t{os}'
+        if   response[TCP].flags == 18: os = 'TCP response received: Likely Windows or Linux-based system.'
+        elif response[TCP].flags == 4:  os = 'TCP reset received: Likely a filtered firewall or closed port.'
+        elif response[TCP].flags == 2:  os = 'SYN response received: Likely a system with an open port (TCP handshake).'
+        elif response[TCP].flags == 1:  os = 'FIN response received: System may be closing the connection (end of session).'
+        else:                           os = 'Unknown TCP flag combination, further analysis required.'
+        self._tcp_result.append(f'\t{os}\n')
 
 
     # UDP ----------------------------------------------------------------------------------------------------
     def _perform_udp_fingerprint(self) -> None:
         """Performs OS fingerprinting using UDP by sending a packet to the target."""
-        print(f"Performing OS fingerprinting with UDP on {self._target_ip}...")
-        packet           = Network._create_udp_ip_packet(self._target_ip, 12345, payload='Hello')
+        print(f'Performing OS fingerprinting with UDP on {self._target_ip}...')
+        packet           = self._prepare_udp_packet()
         self._udp_packet = Network._send_and_receive_single_layer3_packet(packet)
         self._analyse_udp_response()
 
 
+    def _prepare_udp_packet(self) -> Packet:
+        packet    = Network._create_udp_ip_packet(self._target_ip, 12345)
+        packet.id = 0x1042
+        packet    = packet / Raw(load=b'C' * 300)
+        return packet
+
+
     def _analyse_udp_response(self) -> None:
         if self._udp_packet:
-            self._analyse_udp_packet(self._udp_packet)
+            self._analyse_udp_packet()
         else:
             self._udp_result.update({'error': 'No UDP response'})
 
@@ -142,7 +205,6 @@ class OS_Fingerprint:
         code        = self._udp_packet[ICMP].code
         description = self._udp_packet[ICMP].summary
         self._udp_result.update({'type': type, 'code': code, 'desc': description})
-
 
 
     # RESULTS ------------------------------------------------------------------------------------------------
