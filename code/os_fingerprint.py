cfe3bd8ecebcd56e0308125d8ca218463d0a1c90
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-12-25 10:38:29 -0300

    Class to hold the packats was added
    The OS_Fingerprint_Packets class was added to hold the packets and make the principal class clearer. Additionally, it was designed to improve resource usage.

--- code/os_fingerprint.py
+++ code/os_fingerprint.py
@@ -46,54 +46,24 @@ class OS_Fingerprint:
 
     # PACKETS ================================================================================================
 
-    # Sequence generation (SEQ, OPS, WIN, and T1) ------------------------------------------------------------
     def _get_sequence_generation_packets(self) -> Packet:
-        return (
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=1,   options=[('WScale', 10), ('NOP', None), ('MSS', 1460), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b''),]),
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=63,  options=[('MSS', 1400),  ('WScale', 0), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('EOL', None)]),
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=4,   options=[('Timestamp', (0xFFFFFFFF, 0)), ('NOP', None), ('NOP', None), ('WScale', 5), ('NOP', None), ('MSS', 640)]),
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=4,   options=[('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=16,  options=[('MSS', 536), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
-            IP(dst=self._target_ip) / TCP(dport=self._open_port, window=512, options=[('MSS', 265), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0))])
-        )
+        return OS_Fingerprint_Packets._sequence_generation_packets(self._target_ip, self._open_port)
 
 
-    # ICMP echo (IE) -----------------------------------------------------------------------------------------
     def _get_icmp_echo_packets(self) -> Packet:
-        return (
-            IP(dst=self._target_ip, tos=0, flags='DF') / ICMP(type=8, code=9, id=12345, seq=295) / Raw(load=b'\x00' * 120),
-            IP(dst=self._target_ip, tos=4)       /       ICMP(type=8, code=0, id=12346, seq=296) / Raw(load=b'\x00' * 150)
-            )
+        return OS_Fingerprint_Packets._icmp_echo_packets(self._target_ip)
 
 
-    # TCP explicit congestion notification (ECN) -------------------------------------------------------------
     def _get_ecn_syn_packet(self) -> Packet:
-        TCP_OPTIONS        = [('WScale', 10), ('NOP', None), ('MSS', 1460), ('SACKOK', b''), ('NOP', None), ('NOP', None)]
-        packet             = IP(dst=self._target_ip) / TCP(dport=self._open_port, flags="S", window=3, options=TCP_OPTIONS)
-        packet[TCP].flags |= 0x18    # 0x18 = CWR (0b00010000) + ECE (0b00001000)
-        return packet
+        return OS_Fingerprint_Packets._ecn_syn_packet(self._target_ip, self._open_port)
 
 
-    # TCP (T2–T7) --------------------------------------------------------------------------------------------
     def _get_t2_through_t7_tcp_packets(self) -> Packet:
-        COMMON_TCP_OPTIONS        = [('NOP', None), ('MSS', 265), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b'')]
-        COMMOM_WSCALE_AND_OPTIONS = [('WScale', 10)] + COMMON_TCP_OPTIONS    # Equivalent in hex (03030A0102040109080AFFFFFFFF000000000402)
-        return (
-            IP(dst=self._target_ip, flags='DF') / TCP(dport=self._open_port,   flags='',     window=128,   options=COMMOM_WSCALE_AND_OPTIONS),
-            IP(dst=self._target_ip)       /       TCP(dport=self._open_port,   flags='SFUP', window=256,   options=COMMOM_WSCALE_AND_OPTIONS),
-            IP(dst=self._target_ip, flags='DF') / TCP(dport=self._open_port,   flags='A',    window=1024,  options=COMMOM_WSCALE_AND_OPTIONS),
-            IP(dst=self._target_ip)       /       TCP(dport=self._closed_port, flags='S',    window=31337, options=COMMOM_WSCALE_AND_OPTIONS),
-            IP(dst=self._target_ip, flags='DF') / TCP(dport=self._closed_port, flags='A',    window=32768, options=COMMOM_WSCALE_AND_OPTIONS),
-            IP(dst=self._target_ip)       /       TCP(dport=self._closed_port, flags='FPU',  window=65535, options=[('WScale', 15)] + COMMON_TCP_OPTIONS)
-        )
+        return OS_Fingerprint_Packets._t2_through_t7_tcp_packets(self._target_ip, self._open_port, self._closed_port)
 
 
-    # UDP (U1) -----------------------------------------------------------------------------------------------
     def _get_udp_packet(self) -> Packet:
-        packet    = Network._create_udp_ip_packet(self._target_ip, self._closed_port)
-        packet.id = 0x1042
-        packet    = packet / Raw(load=b'C' * 300)
-        return packet
+        return OS_Fingerprint_Packets._udp_packet(self._target_ip, self._closed_port)
 
 
     # RESPONSE TESTS =========================================================================================
@@ -157,3 +127,63 @@ class OS_Fingerprint:
         avg_rate = sum(self._seq_rates) / len(self._seq_rates)
         if avg_rate < 1: return 0
         self._isr = round(8 * log2(avg_rate))
+
+
+
+
+
+class OS_Fingerprint_Packets(): # ============================================================================
+
+    @staticmethod
+    def _sequence_generation_packets(target_ip:str, open_port:int) -> Packet:
+        """ Sequence generation (SEQ, OPS, WIN, and T1) """
+        return (
+            IP(dst=target_ip) / TCP(dport=open_port, window=1,   options=[('WScale', 10), ('NOP', None), ('MSS', 1460), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b''),]),
+            IP(dst=target_ip) / TCP(dport=open_port, window=63,  options=[('MSS', 1400),  ('WScale', 0), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('EOL', None)]),
+            IP(dst=target_ip) / TCP(dport=open_port, window=4,   options=[('Timestamp', (0xFFFFFFFF, 0)), ('NOP', None), ('NOP', None), ('WScale', 5), ('NOP', None), ('MSS', 640)]),
+            IP(dst=target_ip) / TCP(dport=open_port, window=4,   options=[('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
+            IP(dst=target_ip) / TCP(dport=open_port, window=16,  options=[('MSS', 536), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0)), ('WScale', 10), ('EOL', None)]),
+            IP(dst=target_ip) / TCP(dport=open_port, window=512, options=[('MSS', 265), ('SAckOK', b''), ('Timestamp', (0xFFFFFFFF, 0))])
+        )
+
+
+    @staticmethod
+    def _icmp_echo_packets(target_ip:str) -> Packet:
+        """ ICMP echo (IE) """
+        return (
+            IP(dst=target_ip, tos=0, flags='DF') / ICMP(type=8, code=9, id=12345, seq=295) / Raw(load=b'\x00' * 120),
+            IP(dst=target_ip, tos=4)       /       ICMP(type=8, code=0, id=12346, seq=296) / Raw(load=b'\x00' * 150)
+            )
+
+
+    @staticmethod
+    def _ecn_syn_packet(target_ip:str, open_port:int) -> Packet:
+        """ TCP explicit congestion notification (ECN) """
+        TCP_OPTIONS        = [('WScale', 10), ('NOP', None), ('MSS', 1460), ('SACKOK', b''), ('NOP', None), ('NOP', None)]
+        packet             = IP(dst=target_ip) / TCP(dport=open_port, flags="S", window=3, options=TCP_OPTIONS)
+        packet[TCP].flags |= 0x18    # 0x18 = CWR (0b00010000) + ECE (0b00001000)
+        return packet
+
+
+    @staticmethod
+    def _t2_through_t7_tcp_packets(target_ip:str, open_port:int, closed_port:int) -> Packet:
+        """ TCP (T2–T7) """
+        COMMON_TCP_OPTIONS        = [('NOP', None), ('MSS', 265), ('Timestamp', (0xFFFFFFFF, 0)), ('SAckOK', b'')]
+        COMMOM_WSCALE_AND_OPTIONS = [('WScale', 10)] + COMMON_TCP_OPTIONS    # Equivalent in hex (03030A0102040109080AFFFFFFFF000000000402)
+        return (
+            IP(dst=target_ip, flags='DF') / TCP(dport=open_port,   flags='',     window=128,   options=COMMOM_WSCALE_AND_OPTIONS),
+            IP(dst=target_ip)       /       TCP(dport=open_port,   flags='SFUP', window=256,   options=COMMOM_WSCALE_AND_OPTIONS),
+            IP(dst=target_ip, flags='DF') / TCP(dport=open_port,   flags='A',    window=1024,  options=COMMOM_WSCALE_AND_OPTIONS),
+            IP(dst=target_ip)       /       TCP(dport=closed_port, flags='S',    window=31337, options=COMMOM_WSCALE_AND_OPTIONS),
+            IP(dst=target_ip, flags='DF') / TCP(dport=closed_port, flags='A',    window=32768, options=COMMOM_WSCALE_AND_OPTIONS),
+            IP(dst=target_ip)       /       TCP(dport=closed_port, flags='FPU',  window=65535, options=[('WScale', 15)] + COMMON_TCP_OPTIONS)
+        )
+
+
+    @staticmethod
+    def _udp_packet(target_ip:str, closed_port:int) -> Packet:
+        """ UDP (U1) """
+        packet    = Network._create_udp_ip_packet(target_ip, closed_port)
+        packet.id = 0x1042
+        packet    = packet / Raw(load=b'C' * 300)
+        return packet
