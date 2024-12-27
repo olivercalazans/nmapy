4d47bc617d68b2d6b45270b8ba685ce5825305f3
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2024-12-27 16:53:30 -0300

    Update setup.sh

--- code/setup.sh
+++ code/setup.sh
@@ -2,42 +2,49 @@
 
 # Define the file name for the wrapper script
 FILE="dataseeker"
-# Define the path for the installation directory
-DICT="$HOME/.dataseeker/"
+
+# Find the path to the directory containing the os_fingerprint.py file
+PATH1=$(find $HOME -name os_fingerprint.py -exec dirname {} \; 2>/dev/null)
+
+# Get the parent directory of PATH1
+PATH2=${PATH1%/*}
+
+# Define the installation directory path
+DIR="$HOME/.dataseeker/"
 
 # Create the wrapper script with the necessary commands to run the Python script
 echo "#!/bin/bash" > $FILE
-echo "sudo $HOME/.dataseeker/code/seeker/bin/python3 $HOME/.dataseeker/code/main.py \"\$@\"" >> $FILE
+echo "sudo $DIR/seeker/bin/python3 $DIR/main.py \"\$@\"" >> $FILE
 
 # Move the wrapper script to /usr/bin for global access
 sudo mv $FILE /usr/bin
 
-# Give execution permissions to the wrapper script
+# Grant execution permissions to the wrapper script
 sudo chmod +x /usr/bin/$FILE
 
-# Create the main installation directory and subdirectory for code
-mkdir $DICT && mkdir "$DICT/code/"
+# Create the main installation directory and subdirectory for the code
+mkdir -p $DIR
 
-# Move all contents from the '../code' directory into the newly created directory
-mv ../code/* "$DICT/code"
+# Copy all Python files from the found directory to the installation directory
+cp "$PATH1/*.py" $DIR
 
-# Move all other contents from the parent directory to the installation directory, suppressing errors
-mv ../* $DICT 2> /dev/null
+# Copy the LICENSE file from the parent directory to the installation directory, suppressing errors
+cp "$PATH2/LICENSE" $DIR 2> /dev/null
 
-# Install necessary packages for Python virtual environment and pip
+# Install required packages for Python virtual environment and pip
 sudo apt install python3-venv python3-pip -y
 
-# Create a Python virtual environment inside the 'code' directory
-python3 -m venv "$DICT/code/seeker"
+# Create a Python virtual environment inside the installation directory
+python3 -m venv "$DIR/seeker"
 
 # Activate the virtual environment
-source "$DICT/code/seeker/bin/activate"
+source "$DIR/seeker/bin/activate"
 
 # Install the 'scapy' package inside the virtual environment
 pip install scapy
 
-# Deactivate the virtual environment after installation is complete
+# Deactivate the virtual environment after the installation is complete
 deactivate
 
-# Remove the original 'dataseeker' directory, as it's no longer needed
-rm -rf ../../dataseeker
+# Print a completion message
+echo 'INSTALLATION COMPLETED'
