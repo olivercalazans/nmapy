fea374b6a6033bb0ce2ef6ec89261990522408d0
Oliver Calazans <oliver.calazans@gmail.com>
2025-03-06 00:43:41 -0300

    update

--- code/display.py
+++ code/display.py
@@ -1,5 +1,5 @@
 # MIT License
-# Copyright (c) 2024 Oliver Ribeiro Calazans Jeronimo
+# Copyright (c) 2024 Oliver Calazans
 # Repository: https://github.com/olivercalazans/NetXplorer
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software...
 
